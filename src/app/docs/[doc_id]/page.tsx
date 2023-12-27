c8994d82dbacd64fbe9610c39f79707161aae900
Louai Dourov <92324961+Louai-Zokerburg@users.noreply.github.com>
2023-12-26 20:53:07 -0800

    emplemented tiptap editor and liveblocks

--- src/app/docs/[doc_id]/page.tsx
+++ src/app/docs/[doc_id]/page.tsx
@@ -1,10 +1,15 @@
+import { TextEditor } from '@/components/editor/TextEditor'
 import React from 'react'
+
+import { Room } from './Room'
 
 const page = ({ params }: { params: { doc_id: string } }) => {
     return (
-        <section className='ml-0 h-[calc(100vh-60px)] lg:ml-[250px] flex flex-col justify-center items-center gap-y-3 bg-secondary'>
-
-            <div>{params.doc_id}</div>
+        <section className='ml-0 h-[calc(100vh-60px)] lg:ml-[250px] flex flex-col justify-start bg-secondary'>
+            <Room>
+                <TextEditor />
+            </Room>
+            <div className='rounded-md grow w-full h-full p-4 focus:outline-none'></div>
         </section>
     )
 }
